4c7288ceb3f1db5b89b703745c74d47184707894
TheRealImaginary <mazennnnnnnnn@gmail.com>
2018-10-05 18:30:31 +0200

    Removed untested heuristics

--- src/com/ali/me/Main.java
+++ src/com/ali/me/Main.java
@@ -1,11 +1,8 @@
 package com.ali.me;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Stack;
 
-import com.ali.me.cost.heuristic.Heuristic;
 import com.ali.me.cost.heuristic.impl.DragonGlassesHeuristic;
 import com.ali.me.cost.heuristic.impl.NoMoveRestrictionAdmissHeuristic;
 import com.ali.me.cost.heuristic.impl.NoMovementRestrictionHeuristic;
@@ -23,106 +20,99 @@ import com.ali.me.state.impl.TheStateThatKnowsNothing.NorthOfTheWall;
 
 public class Main {
 
-	public static Problem genGrid() {
+    public static Problem genGrid() {
 //		PlaceholderProblem problem = new PlaceholderProblem(30, 30, 20, 100, 100);
-//		 PlaceholderProblem problem = new PlaceholderProblem(20, 20, 20, 100, 5);
-		PlaceholderProblem problem = new PlaceholderProblem(10, 10, 20, 20, 5);
+		 PlaceholderProblem problem = new PlaceholderProblem(20, 20, 20, 100, 5);
+//		PlaceholderProblem problem = new PlaceholderProblem(10, 10, 20, 20, 5);
 //		PlaceholderProblem problem = new PlaceholderProblem(7, 7, 10, 10, 2);
 //		 PlaceholderProblem problem = new PlaceholderProblem(5, 5, 5, 5, 3);
-//		 PlaceholderProblem problem = new PlaceholderProblem(4, 4, 1, 5 , 0);
-		// PlaceholderProblem problem = new PlaceholderProblem(8, 8, 10, 15, 5);
+//        PlaceholderProblem problem = new PlaceholderProblem(4, 4, 1, 5, 0);
+        // PlaceholderProblem problem = new PlaceholderProblem(8, 8, 10, 15, 5);
 
-		NorthOfTheWall[][] grid = ((TheStateThatKnowsNothing) problem.getInitialState()).getGrid();
-		System.err.println("Initial Grid");
-		for (int i = 0; i < grid.length; i++)
-			System.err.println(Arrays.toString(grid[i]));
-		return problem;
-	}
+        NorthOfTheWall[][] grid = ((TheStateThatKnowsNothing) problem.getInitialState()).getGrid();
+        System.err.println("Initial Grid");
+        for (int i = 0; i < grid.length; i++)
+            System.err.println(Arrays.toString(grid[i]));
+        return problem;
+    }
 
-	public static void search(Problem problem, String strategy, boolean visualize) {
-		strategy = strategy.toLowerCase();
-		State goalState = null;
-		if (strategy.equals("dfs")) {
-			DFSSearchStrategy dfs = new DFSSearchStrategy();
-			goalState = dfs.search(problem);
-		}
-		if (strategy.equals("id")) {
-			IterDeepSeatchStrategy dfs = new IterDeepSeatchStrategy();
-			goalState = dfs.search(problem, 0);
-		}
-		if (strategy.equals("bfs")) {
-			BFSSearchStrategy bfs = new BFSSearchStrategy();
-			goalState = bfs.search(problem);
-		}
-		if (strategy.equals("ucs")) {
-			UCSSearchStrategy ucs = new UCSSearchStrategy();
-			goalState = ucs.search(problem);
-		}
-		if (strategy.equals("gbfsh1")) {
-			GBFSSearchStrategy gbfs = new GBFSSearchStrategy(new DragonGlassesHeuristic());
-			goalState = gbfs.search(problem);
-		}
-		if (strategy.equals("gbfsh2")) {
-			GBFSSearchStrategy gbfs = new GBFSSearchStrategy(new NoMovementRestrictionHeuristic());
-			goalState = gbfs.search(problem);
-		}
-		if(strategy.equals("astarh1")) {
-			AStarSearchStrategy astar = new AStarSearchStrategy(new DragonGlassesHeuristic());
-			goalState = astar.search(problem);
-		}
-		if(strategy.equals("astarh2")) {
-			AStarSearchStrategy astar = new AStarSearchStrategy(new NoMovementRestrictionHeuristic());
-			goalState = astar.search(problem);
-		}
-		if(strategy.equals("astarh3")) {
-			AStarSearchStrategy astar = new AStarSearchStrategy(new NoMoveRestrictionAdmissHeuristic());
-			goalState = astar.search(problem);
-		}
-		if (goalState == null)
-			System.err.println("Found No Solution");
-		else {
-			TheStateThatKnowsNothing nothing = (TheStateThatKnowsNothing) goalState;
-			pathPrint(nothing);
-			// System.err.println("Goal state");
-			System.err.println(nothing);
-			System.err.println(nothing.getDepth());
-			NorthOfTheWall[][] grid = nothing.getGrid();
-			for (int i = 0; i < grid.length; i++)
-				System.out.println(Arrays.toString(grid[i]));
-		}
-		
-	}
+    public static void search(Problem problem, String strategy, boolean visualize) {
+        strategy = strategy.toLowerCase();
+        State goalState = null;
+        if (strategy.equals("dfs")) {
+            DFSSearchStrategy dfs = new DFSSearchStrategy();
+            goalState = dfs.search(problem);
+        }
+        if (strategy.equals("id")) {
+            IterDeepSeatchStrategy dfs = new IterDeepSeatchStrategy();
+            goalState = dfs.search(problem, 0);
+        }
+        if (strategy.equals("bfs")) {
+            BFSSearchStrategy bfs = new BFSSearchStrategy();
+            goalState = bfs.search(problem);
+        }
+        if (strategy.equals("ucs")) {
+            UCSSearchStrategy ucs = new UCSSearchStrategy();
+            goalState = ucs.search(problem);
+        }
+        if (strategy.equals("gbfsh1")) {
+            GBFSSearchStrategy gbfs = new GBFSSearchStrategy(new DragonGlassesHeuristic());
+            goalState = gbfs.search(problem);
+        }
+        if (strategy.equals("gbfsh2")) {
+            GBFSSearchStrategy gbfs = new GBFSSearchStrategy(new NoMovementRestrictionHeuristic());
+            goalState = gbfs.search(problem);
+        }
+        if (strategy.equals("astarh1")) {
+            AStarSearchStrategy astar = new AStarSearchStrategy(new DragonGlassesHeuristic());
+            goalState = astar.search(problem);
+        }
+        if (goalState == null)
+            System.err.println("Found No Solution");
+        else {
+            TheStateThatKnowsNothing nothing = (TheStateThatKnowsNothing) goalState;
+//            pathPrint(nothing);
+            // System.err.println("Goal state");
+            System.err.println(nothing);
+//            System.err.println(nothing.getDepth());
+            NorthOfTheWall[][] grid = nothing.getGrid();
+            for (int i = 0; i < grid.length; i++)
+                System.out.println(Arrays.toString(grid[i]));
+        }
 
-	public static void pathPrint(TheStateThatKnowsNothing state) {
-		Stack<TheStateThatKnowsNothing> stack = new Stack<TheStateThatKnowsNothing>();
-		TheStateThatKnowsNothing currState = state;
+    }
 
-		while (currState.getParent() != null) {
-			stack.push((TheStateThatKnowsNothing) currState.getParent());
-			currState = (TheStateThatKnowsNothing) currState.getParent();
-		}
-		while (!stack.isEmpty()) {
-			TheStateThatKnowsNothing currStateStack = stack.pop();
-			NorthOfTheWall[][] grid = currStateStack.getGrid();
-			for (int i = 0; i < grid.length; i++)
-				System.out.println(Arrays.toString(grid[i]));
-			System.out.println();
-			System.out.println();
-		}
-	}
+    public static void pathPrint(TheStateThatKnowsNothing state) {
+        Stack<TheStateThatKnowsNothing> stack = new Stack<TheStateThatKnowsNothing>();
+        TheStateThatKnowsNothing currState = state;
 
-	public static void main(String[] args) {
+        while (currState.getParent() != null) {
+            stack.push((TheStateThatKnowsNothing) currState.getParent());
+            currState = (TheStateThatKnowsNothing) currState.getParent();
+        }
+        while (!stack.isEmpty()) {
+            TheStateThatKnowsNothing currStateStack = stack.pop();
+            NorthOfTheWall[][] grid = currStateStack.getGrid();
+            for (int i = 0; i < grid.length; i++)
+                System.out.println(Arrays.toString(grid[i]));
+            System.out.println();
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
 //		new Thread(null, new Runnable() {
 //			@Override
 //			public void run() {
 //				
 //			}
 //		}, "Increase Stack Size", 1 << 27).start();
-		long time = System.currentTimeMillis();
-		Problem problem = genGrid();
-		search(problem, "astarh3", false);
-		long time1 = System.currentTimeMillis();
-		System.out.println((time1 - time)/1000.0);
+        long time = System.currentTimeMillis();
+        Problem problem = genGrid();
+        search(problem, "ucs", false);
+        search(problem, "astarh1", false);
+        long time1 = System.currentTimeMillis();
+        System.out.println((time1 - time) / 1000.0);
 //		System.out.println("___________________________________________________________________________");
 //		search(problem, "bfs", false);
 //		Heuristic heuristic = new DragonGlassesHeuristic();
@@ -131,5 +121,5 @@ public class Main {
 //		System.out.println(heuristic.heuristicCost(problem.getInitialState()));
 //		List<State> list = problem.expand(problem.getInitialState());
 //		System.out.println(heuristic1.heuristicCost(problem.getInitialState()));
-	}
+    }
 }
