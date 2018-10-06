84cdd5aa676ed7991a6005ab7201a509bb61cd50
TheRealImaginary <mazennnnnnnnn@gmail.com>
2018-10-06 21:26:30 +0200

    Added GUI and resources and restructured Main.java

---
+++ src/com/ali/me/Main.java
@@ -0,0 +1,94 @@
+package com.ali.me;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+import com.ali.me.cost.heuristic.impl.DragonGlassesHeuristic;
+import com.ali.me.problem.Problem;
+import com.ali.me.problem.impl.PlaceholderProblem;
+import com.ali.me.search.impl.*;
+import com.ali.me.state.State;
+import com.ali.me.state.impl.TheStateThatKnowsNothing;
+import com.ali.me.state.impl.TheStateThatKnowsNothing.NorthOfTheWall;
+import com.ali.me.view.SolutionViewer;
+
+public class Main {
+
+    private static Problem genGrid() {
+		PlaceholderProblem problem = new PlaceholderProblem(30, 30, 20, 100, 100);
+//        PlaceholderProblem problem = new PlaceholderProblem(20, 20, 20, 100, 5);
+//		PlaceholderProblem problem = new PlaceholderProblem(10, 10, 20, 20, 5);
+//        PlaceholderProblem problem = new PlaceholderProblem(7, 7, 10, 10, 2);
+//        PlaceholderProblem problem = new PlaceholderProblem(7, 7, 10, 12, 2);
+//        PlaceholderProblem problem = new PlaceholderProblem(7, 8, 10, 12, 2);
+//        PlaceholderProblem problem = new PlaceholderProblem(5, 5, 5, 5, 3);
+//        PlaceholderProblem problem = new PlaceholderProblem(4, 4, 1, 5, 0);
+//        PlaceholderProblem problem = new PlaceholderProblem(8, 8, 10, 15, 5);
+
+        NorthOfTheWall[][] grid = ((TheStateThatKnowsNothing) problem.getInitialState()).getGrid();
+        System.err.println("Initial Grid");
+        for (int i = 0; i < grid.length; i++)
+            System.err.println(Arrays.toString(grid[i]));
+        return problem;
+    }
+
+    private static void search(Problem problem, String strategy, boolean visualize) {
+        strategy = strategy.toLowerCase();
+        State goalState = null;
+        if (strategy.equals("dfs")) {
+            DFSSearchStrategy dfs = new DFSSearchStrategy();
+            goalState = dfs.search(problem);
+        }
+        if (strategy.equals("bfs")) {
+            BFSSearchStrategy bfs = new BFSSearchStrategy();
+            goalState = bfs.search(problem);
+        }
+        if (strategy.equals("id")) {
+            IterativeDeepeningSearchStrategy id = new IterativeDeepeningSearchStrategy();
+            goalState = id.search(problem);
+        }
+        if (strategy.equals("ucs")) {
+            UCSSearchStrategy ucs = new UCSSearchStrategy();
+            goalState = ucs.search(problem);
+        }
+        if (strategy.equals("gbfsh1")) {
+            GBFSSearchStrategy gbfs = new GBFSSearchStrategy(new DragonGlassesHeuristic());
+            goalState = gbfs.search(problem);
+        }
+        if (strategy.equals("astarh1")) {
+            AStarSearchStrategy astar = new AStarSearchStrategy(new DragonGlassesHeuristic());
+            goalState = astar.search(problem);
+        }
+        if (goalState == null)
+            System.err.println("Found No Solution");
+        else {
+            if (visualize) {
+                SolutionViewer sv = new SolutionViewer("Solution Viewer", getPath(goalState));
+            }
+        }
+    }
+
+    private static List<State> getPath(State state) {
+        Stack<State> stack = new Stack<>();
+
+        while (state != null) {
+            stack.push(state);
+            state = state.getParent();
+        }
+        List<State> path = new ArrayList<>();
+        while (!stack.isEmpty()) path.add(stack.pop());
+        return path;
+    }
+
+    public static void main(String[] args) {
+        new Thread(null, () -> {
+            Problem problem = genGrid();
+            long start = System.currentTimeMillis();
+            search(problem, "dfs", true);
+            long end = System.currentTimeMillis();
+            System.err.println((end - start) / 1000.0);
+        }, "Increase Stack Size", 1 << 27).start();
+    }
+}
---
+++ src/com/ali/me/view/SolutionViewer.java
@@ -0,0 +1,235 @@
+package com.ali.me.view;
+
+import com.ali.me.action.Action;
+import com.ali.me.action.impl.PlaceholderAction;
+import com.ali.me.state.State;
+import com.ali.me.state.impl.TheStateThatKnowsNothing;
+import com.ali.me.state.impl.TheStateThatKnowsNothing.NorthOfTheWall;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+public class SolutionViewer extends JFrame {
+
+    private class Controls extends JPanel {
+        private JLabel pathCostLabel;
+        private JLabel actionTakenLabel;
+        private JLabel dragonGlassesLabel;
+        private JLabel heuristicCostLabel;
+
+        private JButton step;
+        private JButton undo;
+
+        private Controls() {
+            this.pathCostLabel = new JLabel("Path Cost: 0 | ");
+            this.dragonGlassesLabel = new JLabel("Dragon Glasses: 0 | ");
+            this.actionTakenLabel = new JLabel("Action Taken: None | ");
+            this.heuristicCostLabel = new JLabel("Heusitic Cost: 0");
+
+            this.step = new JButton("Step");
+            this.undo = new JButton("Undo");
+
+            this.add(this.pathCostLabel);
+            this.add(this.dragonGlassesLabel);
+            this.add(this.actionTakenLabel);
+            this.add(this.heuristicCostLabel);
+
+            this.add(this.step);
+            this.add(this.undo);
+        }
+
+        private void setPathCost(int pathCost) {
+            this.pathCostLabel.setText("Path Cost: " + pathCost + " | ");
+        }
+
+        private void setDragonGlasses(int dragonGlasses) {
+            this.dragonGlassesLabel.setText("Dragon Glasses: " + dragonGlasses + " | ");
+        }
+
+        private void setActionTaken(Action action) {
+            String actionTaken = null;
+            if (action == null) actionTaken = "None";
+            else actionTaken = action.getActionString();
+            this.actionTakenLabel.setText("Action Taken: " + actionTaken + " | ");
+        }
+
+        private void setHeuristicCost(int heuriticCost) {
+            this.heuristicCostLabel.setText("Heuristic Cost: " + heuriticCost);
+        }
+    }
+
+    private class MainView extends JPanel {
+        private final ImageIcon JON_ICON = new ImageIcon("resources/jon_snow.jpeg");
+        private final ImageIcon WHITE_WALKER_ICON = new ImageIcon("resources/white_walker.jpeg");
+        private final ImageIcon DRAGON_STONE_ICON = new ImageIcon("resources/throne.jpeg");
+        private final ImageIcon OBSTACLE_ICON = new ImageIcon("resources/obstacle.png");
+
+        private NorthOfTheWall[][] initialGrid;
+        private JButton[][] grid;
+        private int row, col, numberOfRows, numberOfColumns;
+
+        private MainView(State state) {
+            this.initialGrid = ((TheStateThatKnowsNothing) state).getGrid();
+            this.numberOfRows = this.initialGrid.length;
+            this.numberOfColumns = this.initialGrid[0].length;
+
+            this.setLayout(new GridLayout(numberOfRows, numberOfColumns));
+
+            grid = new JButton[numberOfRows][numberOfColumns];
+            for (int row = 0; row < numberOfRows; row++)
+                for (int col = 0; col < numberOfColumns; col++) {
+                    ImageIcon imageIcon = null;
+                    switch (this.initialGrid[row][col]) {
+                        case W:
+                            imageIcon = WHITE_WALKER_ICON;
+                            break;
+                        case J:
+                            imageIcon = JON_ICON;
+                            this.row = row;
+                            this.col = col;
+                            break;
+                        case D:
+                            imageIcon = DRAGON_STONE_ICON;
+                            break;
+                        case O:
+                            imageIcon = OBSTACLE_ICON;
+                            break;
+                    }
+                    grid[row][col] = new JButton(imageIcon);
+                    grid[row][col].setContentAreaFilled(false);
+                    grid[row][col].setBorderPainted(false);
+                    this.add(grid[row][col]);
+                }
+        }
+
+        private void moveLeft() {
+            if (this.grid[this.row][this.col].getIcon() == JON_ICON)
+                this.grid[this.row][this.col].setIcon(null);
+            this.col--;
+            if (this.grid[this.row][this.col].getIcon() == null)
+                this.grid[this.row][this.col].setIcon(JON_ICON);
+        }
+
+        private void moveRight() {
+            if (this.grid[this.row][this.col].getIcon() == JON_ICON)
+                this.grid[this.row][this.col].setIcon(null);
+            this.col++;
+            if (this.grid[this.row][this.col].getIcon() == null)
+                this.grid[this.row][this.col].setIcon(JON_ICON);
+        }
+
+        private void moveUp() {
+            if (this.grid[this.row][this.col].getIcon() == JON_ICON)
+                this.grid[this.row][this.col].setIcon(null);
+            this.row--;
+            if (this.grid[this.row][this.col].getIcon() == null)
+                this.grid[this.row][this.col].setIcon(JON_ICON);
+        }
+
+        private void moveDown() {
+            if (this.grid[this.row][this.col].getIcon() == JON_ICON)
+                this.grid[this.row][this.col].setIcon(null);
+            this.row++;
+            if (this.grid[this.row][this.col].getIcon() == null)
+                this.grid[this.row][this.col].setIcon(JON_ICON);
+        }
+
+        private void attack(boolean revive) {
+            int[] dx = {1, 0, -1, 0};
+            int[] dy = {0, 1, 0, -1};
+            for (int i = 0; i < dx.length; i++) {
+                int r = row + dx[i], c = col + dy[i];
+                if (r >= 0 && r < this.numberOfRows && c >= 0 && c < this.numberOfColumns && this.initialGrid[r][c] == NorthOfTheWall.W)
+                    if (!revive) this.grid[r][c].setIcon(null);
+                    else this.grid[r][c].setIcon(WHITE_WALKER_ICON);
+            }
+        }
+
+        private void pickup() {
+        }
+    }
+
+    private int currentStep;
+    private List<State> path;
+    private Controls controls;
+    private MainView mainView;
+
+    public SolutionViewer(String title, List<State> path) throws HeadlessException {
+        super(title);
+        this.currentStep = 0;
+        this.path = path;
+
+        this.setSize(1000, 700);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.controls = new Controls();
+        this.mainView = new MainView(this.path.get(0));
+
+        this.add(this.mainView, BorderLayout.CENTER);
+        this.add(this.controls, BorderLayout.NORTH);
+
+        this.controls.step.addActionListener((actionEvent) -> {
+            if (currentStep + 1 == path.size()) return;
+            State nextState = this.path.get(++currentStep);
+            int dragonGlasses = ((TheStateThatKnowsNothing) nextState).getDragonGlasses();
+            switch (nextState.getAction().getAction()) {
+                case PlaceholderAction.MOVE_LEFT:
+                    this.mainView.moveLeft();
+                    break;
+                case PlaceholderAction.MOVE_RIGHT:
+                    this.mainView.moveRight();
+                    break;
+                case PlaceholderAction.MOVE_UP:
+                    this.mainView.moveUp();
+                    break;
+                case PlaceholderAction.MOVE_DOWN:
+                    this.mainView.moveDown();
+                    break;
+                case PlaceholderAction.ATTACK:
+                    this.mainView.attack(false);
+                    break;
+                case PlaceholderAction.PICK_UP:
+                    this.mainView.pickup();
+                    break;
+            }
+            this.controls.setPathCost(nextState.getPathCost());
+            this.controls.setActionTaken(nextState.getAction());
+            this.controls.setDragonGlasses(dragonGlasses);
+            this.controls.setHeuristicCost(nextState.getHeuristicCost());
+        });
+        this.controls.undo.addActionListener((actionEvent) -> {
+            if (currentStep == 0) return;
+            State state = this.path.get(currentStep--);
+            switch (state.getAction().getAction()) {
+                case PlaceholderAction.MOVE_LEFT:
+                    this.mainView.moveRight();
+                    break;
+                case PlaceholderAction.MOVE_RIGHT:
+                    this.mainView.moveLeft();
+                    break;
+                case PlaceholderAction.MOVE_UP:
+                    this.mainView.moveDown();
+                    break;
+                case PlaceholderAction.MOVE_DOWN:
+                    this.mainView.moveUp();
+                    break;
+                case PlaceholderAction.ATTACK:
+                    this.mainView.attack(true);
+                    break;
+                case PlaceholderAction.PICK_UP:
+                    this.mainView.pickup();
+                    break;
+            }
+            State previousState = this.path.get(currentStep);
+            int dragonGlasses = ((TheStateThatKnowsNothing) previousState).getDragonGlasses();
+            this.controls.setPathCost(previousState.getPathCost());
+            this.controls.setActionTaken(previousState.getAction());
+            this.controls.setDragonGlasses(dragonGlasses);
+            this.controls.setHeuristicCost(previousState.getHeuristicCost());
+        });
+
+        this.setVisible(true);
+    }
+}
