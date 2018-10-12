9dcaa9c87fde52744698119ffe1e019520c425ff
TheRealImaginary <mazennnnnnnnn@gmail.com>
2018-10-12 20:04:56 +0200

    Linked method in SearchStrategy Documentation

--- src/com/ali/me/search/SearchStrategy.java
+++ src/com/ali/me/search/SearchStrategy.java
@@ -8,6 +8,12 @@ import com.ali.me.state.State;
  * must implement and define the behaviour of the following abstract method.
  */
 public abstract class SearchStrategy {
-
+    /**
+     * Searches the state space of the given problem, starting from its Initial state
+     * and returns either a Goal state satisfying {@link Problem#isGoal} method or null
+     * if no solution is found.
+     * @param problem The problem for which a solution is to be searched
+     * @return A Goal state satisfying {@link Problem#isGoal} method or null if no solution is found
+     */
     public abstract State search(Problem problem);
 }
