ce41a8a799693977626109c718a10454daf2b4f9
TheRealImaginary <mazennnnnnnnn@gmail.com>
2018-10-12 19:30:52 +0200

    Added Documentation for Search Strategy Class

--- src/com/ali/me/search/SearchStrategy.java
+++ src/com/ali/me/search/SearchStrategy.java
@@ -3,6 +3,10 @@ package com.ali.me.search;
 import com.ali.me.problem.Problem;
 import com.ali.me.state.State;
 
+/**
+ * This class describes the structure of a Search Strategy. Implementing Sub-classes
+ * must implement and define the behaviour of the following abstract method.
+ */
 public abstract class SearchStrategy {
 
     public abstract State search(Problem problem);
