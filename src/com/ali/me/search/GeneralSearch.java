fe12be020843e7e501b43d9d72539b04af9a205a
TheRealImaginary <mazennnnnnnnn@gmail.com>
2018-10-14 20:37:41 +0200

    Added number of expanded Nodes

--- src/com/ali/me/search/GeneralSearch.java
+++ src/com/ali/me/search/GeneralSearch.java
@@ -5,11 +5,15 @@ import com.ali.me.state.State;
 
 public class GeneralSearch {
 
+    public static int expandedNodes;
+
     public static State search(Problem problem, SearchStrategy searchStrategy) {
+        expandedNodes = 0;
         searchStrategy.add(problem.getInitialState());
         while (!searchStrategy.isEmpty()) {
             State state = searchStrategy.pop();
             if (state == null) continue;
+            expandedNodes++;
             if (problem.isGoal(state)) return state;
             searchStrategy.expand(problem, state);
         }
