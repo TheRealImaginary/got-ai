d9a662964afff9c02e4a6043fd2bdfc8dfd457c8
TheRealImaginary <mazennnnnnnnn@gmail.com>
2018-10-18 22:14:49 +0200

    Added Dragon Glass Heuristic Docs

--- src/com/ali/me/cost/heuristic/impl/DragonGlassesHeuristic.java
+++ src/com/ali/me/cost/heuristic/impl/DragonGlassesHeuristic.java
@@ -4,7 +4,17 @@ import com.ali.me.cost.heuristic.Heuristic;
 import com.ali.me.state.State;
 import com.ali.me.state.impl.SaveWesterosState;
 
+/**
+ * Calculates an estimate of the number of Dragon Glasses needed.
+ */
 public class DragonGlassesHeuristic implements Heuristic {
+    /**
+     * Returns an estimate of the number of Dragon Glasses needed. It does
+     * so by counting the number of White Walkers and dividing this number
+     * by `3`. This assumes Jon can always kill 3 White Walkers at a time.
+     * @param state
+     * @return
+     */
     @Override
     public int heuristicCost(State state) {
         SaveWesterosState.NorthOfTheWall[][] grid = ((SaveWesterosState) state).getGrid();
