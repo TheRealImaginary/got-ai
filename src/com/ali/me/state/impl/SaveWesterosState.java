e3e18104c8446b4c0ed24c8503b694acbff604e9
TheRealImaginary <mazennnnnnnnn@gmail.com>
2018-10-18 22:30:19 +0200

    Added Docs to SaveWesteros State

--- src/com/ali/me/state/impl/SaveWesterosState.java
+++ src/com/ali/me/state/impl/SaveWesterosState.java
@@ -5,26 +5,81 @@ import com.ali.me.state.State;
 
 import java.util.Arrays;
 
+/**
+ * Represents a State for the SaveWesteros Problem.
+ */
 public class SaveWesterosState extends State {
 
+    /**
+     * Enum for Cell Type
+     */
     public enum NorthOfTheWall {
+        /**
+         * Jon
+         */
         J(0),
+        /**
+         * Empty
+         */
         E(1),
+        /**
+         * Obstacle
+         */
         O(2),
+        /**
+         * White Walker
+         */
         W(3),
+        /**
+         * Dragon Stone
+         */
         D(4);
+        /**
+         * ID Used in comparing the grid
+         */
         public int id;
 
+        /**
+         * @param id ID of this cell type
+         */
         NorthOfTheWall(int id) {
             this.id = id;
         }
     }
 
+    /**
+     * Row that Jon is currently in.
+     */
     private int row;
+
+    /**
+     * Column that Jon is currently in.
+     */
     private int column;
+
+    /**
+     * Number of Dragon Glasses that Jon is
+     * currently carrying.
+     */
     private int dragonGlasses;
+
+    /**
+     * The Grid.
+     */
     private NorthOfTheWall[][] grid;
 
+    /**
+     * Creates an Instance of the SaveWesteros State.
+     * @param depth Depth of this state.
+     * @param parent Parent of this state.
+     * @param pathCost Path Cost from root (Initial state) to this one.
+     * @param action Previous Action taken.
+     * @param heuristicCost Heuristic cost.
+     * @param row Row that Jon is currently in.
+     * @param column Column that Jon is currently in.
+     * @param dragonGlasses Number of Dragon Glasses that Jon is currently carrying.
+     * @param grid The Grid.
+     */
     public SaveWesterosState(int depth, State parent, int pathCost, Action action, int heuristicCost, int row, int column, int dragonGlasses, NorthOfTheWall[][] grid) {
         super(depth, parent, pathCost, action, heuristicCost);
         this.row = row;
@@ -33,22 +88,42 @@ public class SaveWesterosState extends State {
         this.grid = grid;
     }
 
+    /**
+     * Getter for {@link SaveWesterosState#row}
+     * @return
+     */
     public int getRow() {
         return row;
     }
 
+    /**
+     * Getter for {@link SaveWesterosState#column}
+     * @return
+     */
     public int getColumn() {
         return column;
     }
 
+    /**
+     * Getter for {@link SaveWesterosState#dragonGlasses}
+     * @return
+     */
     public int getDragonGlasses() {
         return dragonGlasses;
     }
 
+    /**
+     * Getter for {@link SaveWesterosState#grid}
+     * @return
+     */
     public NorthOfTheWall[][] getGrid() {
         return grid;
     }
 
+    /**
+     * Returns a string representation of this state.
+     * @return
+     */
     @Override
     public String toString() {
         return "SaveWesterosState{" +
@@ -60,6 +135,12 @@ public class SaveWesterosState extends State {
                 '}';
     }
 
+    /**
+     * Compares two states. This is used in Search Strategy
+     * to check for previously visited States and their costs.
+     * @param state
+     * @return
+     */
     @Override
     public int compareTo(State state) {
         SaveWesterosState otherState = (SaveWesterosState) state;
