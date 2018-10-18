2e92e6f3aa9752a6a4fe0ce045320245f9f1d4ae
TheRealImaginary <mazennnnnnnnn@gmail.com>
2018-10-18 22:23:23 +0200

    Added Docs to State

--- src/com/ali/me/state/State.java
+++ src/com/ali/me/state/State.java
@@ -2,14 +2,44 @@ package com.ali.me.state;
 
 import com.ali.me.action.Action;
 
+/**
+ * Represent a Node in the Search Tree.
+ */
 public abstract class State implements Comparable<State> {
 
+    /**
+     * Depth of this node in the Search Tree.
+     */
     private int depth;
+
+    /**
+     * Parent of this Node.
+     */
     private State parent;
+
+    /**
+     * Path Cost from root (Initial) node to this one.
+     */
     private int pathCost;
+
+    /**
+     * Action taken at the previous node, hence resulting in this one.
+     */
     private Action action;
+
+    /**
+     * Heuristic Cost of this node.
+     */
     private int heuristicCost;
 
+    /**
+     * Populates the above properties.
+     * @param depth
+     * @param parent
+     * @param pathCost
+     * @param action
+     * @param heuristicCost
+     */
     public State(int depth, State parent, int pathCost, Action action, int heuristicCost) {
         this.depth = depth;
         this.parent = parent;
@@ -18,30 +48,58 @@ public abstract class State implements Comparable<State> {
         this.heuristicCost = heuristicCost;
     }
 
+    /**
+     * Getter for {@link State#depth}
+     * @return
+     */
     public int getDepth() {
         return depth;
     }
 
+    /**
+     * Getter for {@link State#pathCost}
+     * @return
+     */
     public int getPathCost() {
         return pathCost;
     }
 
+    /**
+     * Getter for {@link State#heuristicCost}
+     * @return
+     */
     public int getHeuristicCost() {
         return heuristicCost;
     }
 
+    /**
+     * Setter for {@link State#heuristicCost}
+     * @param heuristicCost
+     */
     public void setHeuristicCost(int heuristicCost) {
         this.heuristicCost = heuristicCost;
     }
 
+    /**
+     * Getter for {@link State#parent}
+     * @return
+     */
     public State getParent() {
         return parent;
     }
 
+    /**
+     * Getter for {@link State#action}
+     * @return
+     */
     public Action getAction() {
         return action;
     }
 
+    /**
+     * Returns a string representation of this node.
+     * @return
+     */
     @Override
     public String toString() {
         // We avoid printing the parent because printing would be long
